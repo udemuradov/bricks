ec62ceba878feb1a34fa36dc32090481f0a61ac4
su <udemuradovv@gmail.com>
2025-01-19 13:50:31 +0500

    feat: upgrade bricks ver.

--- bricks/bloc/hooks/pre_gen.dart
+++ bricks/bloc/hooks/pre_gen.dart
@@ -4,8 +4,8 @@ Future<void> run(HookContext context) async {
   final style = context.vars['style'];
   context.vars = {
     ...context.vars,
-    'use_equatable': style == 'equatable',
     'use_freezed_union': style == 'freezed_union',
+    'use_equatable': style == 'equatable',
     'use_freezed_sealed': style == 'freezed_sealed',
   };
 }
